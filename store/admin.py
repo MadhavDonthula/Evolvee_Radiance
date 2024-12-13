4a124288d87e1381441c647c5eb23713aecab9af
MadhavDonthula <mdonthula98@gmail.com>
2024-12-12 18:00:23 -0800

    first

---
+++ store/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated', 'category']
+    prepopulated_fields = {'slug': ('name',)}
---
+++ store/models.py
@@ -0,0 +1,38 @@
+# store/models.py
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='products/')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+
+# store/admin.py
+
+# store/views.py
+
+# store/urls.py
+
+
+# lip_products/urls.py
+
+# lip_products/settings.py (add these settings)
