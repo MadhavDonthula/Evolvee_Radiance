81788badb60902afa1d630a579d40c93c7ebcdb9
MadhavDonthula <mdonthula98@gmail.com>
2025-01-09 11:07:07 -0800

    images2

--- lip_products/settings.py
+++ lip_products/settings.py
@@ -120,11 +120,14 @@ DATABASES["default"] = dj_database_url.parse("postgresql://evolveeradiance_user:
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
