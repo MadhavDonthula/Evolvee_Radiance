babf46ed2335552605fa8486ec1e234edd416203
MadhavDonthula <mdonthula98@gmail.com>
2025-04-23 21:26:16 -0700

    check

--- store/views.py
+++ store/views.py
@@ -12,17 +12,9 @@ from decimal import Decimal
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
-import boto3
 from django.conf import settings
 
-# Initialize S3 client
-s3_client = boto3.client(
-    's3',
-    endpoint_url=settings.AWS_S3_ENDPOINT_URL,
-    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-    region_name=settings.AWS_S3_REGION_NAME
-)
+
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
