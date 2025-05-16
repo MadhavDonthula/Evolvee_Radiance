7dc276427fd0c3c9f5bcbd5ab1425c6227fd0094
MadhavDonthula <mdonthula98@gmail.com>
2025-05-15 17:48:58 -0700

    changes

--- lip_products/settings.py
+++ lip_products/settings.py
@@ -1,3 +1,102 @@
+# import os
+# from pathlib import Path
+# import environ
+
+# # Initialize environment variables
+# env = environ.Env(
+#     DEBUG=(bool, False)
+# )
+# environ.Env.read_env(os.path.join(Path(__file__).resolve().parent.parent, '.env'))
+
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SECRET_KEY = env('SECRET_KEY')
+# DEBUG = env('DEBUG')
+# ALLOWED_HOSTS = ['*']
+
+# # Application definition
+# INSTALLED_APPS = [
+#     "django.contrib.admin",
+#     "django.contrib.auth",
+#     "django.contrib.contenttypes",
+#     "django.contrib.sessions",
+#     "django.contrib.messages",
+#     "django.contrib.staticfiles",
+#     "store",
+# ]
+
+# MIDDLEWARE = [
+#     "django.middleware.security.SecurityMiddleware",
+#     "whitenoise.middleware.WhiteNoiseMiddleware",  # Must be just after SecurityMiddleware
+#     "django.contrib.sessions.middleware.SessionMiddleware",
+#     "django.middleware.common.CommonMiddleware",
+#     "django.middleware.csrf.CsrfViewMiddleware",
+#     "django.contrib.auth.middleware.AuthenticationMiddleware",
+#     "django.contrib.messages.middleware.MessageMiddleware",
+#     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+# ]
+
+# ROOT_URLCONF = "lip_products.urls"
+
+# TEMPLATES = [
+#     {
+#         "BACKEND": "django.template.backends.django.DjangoTemplates",
+#         "DIRS": [BASE_DIR / "templates"],
+#         "APP_DIRS": True,
+#         "OPTIONS": {
+#             "context_processors": [
+#                 "django.template.context_processors.debug",
+#                 "django.template.context_processors.request",
+#                 "django.contrib.auth.context_processors.auth",
+#                 "django.contrib.messages.context_processors.messages",
+#             ],
+#         },
+#     },
+# ]
+
+# WSGI_APPLICATION = "lip_products.wsgi.application"
+
+# # Database
+# DATABASES = {
+#     "default": env.db(),
+# }
+
+# # Password validation
+# AUTH_PASSWORD_VALIDATORS = [
+#     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
+#     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+#     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+#     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+# ]
+
+# # Internationalization
+# LANGUAGE_CODE = "en-us"
+# TIME_ZONE = "UTC"
+# USE_I18N = True
+# USE_TZ = True
+
+# # Static files (CSS, JS, Images)
+# STATIC_URL = "/static/"
+# STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# # Media files
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = BASE_DIR / "media"
+
+# # Default primary key field type
+# DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
+
+
+
+#### NEWW####
+
+
 import os
 from pathlib import Path
 import environ
@@ -9,8 +108,8 @@ env = environ.Env(
 environ.Env.read_env(os.path.join(Path(__file__).resolve().parent.parent, '.env'))
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-SECRET_KEY = env('SECRET_KEY')
+DATABASE_URL = "postgresql://ev5_user:jrjwMQbZvJSEsGhKIAn1nSWaQeEdgzZj@dpg-d04rt5p5pdvs73acibh0-a.oregon-postgres.render.com/ev5"
+SECRET_KEY = 'ys-optx20q8kkmq$6z_z2dh6($d7y=@2q7b%_yzv20+fke*i05'
 DEBUG = env('DEBUG')
 ALLOWED_HOSTS = ['*']
 
@@ -56,10 +155,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "lip_products.wsgi.application"
 
-# Database
 DATABASES = {
-    "default": env.db(),
+    "default": env.db(default="postgresql://ev5_user:jrjwMQbZvJSEsGhKIAn1nSWaQeEdgzZj@dpg-d04rt5p5pdvs73acibh0-a.oregon-postgres.render.com/ev5"),
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
